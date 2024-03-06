332d1f451ce3b9f84396a0a4167b6ba7d608d70a
supalerk-ar66 <supalerk_ar66@live.rmutl.ac.th>
2024-03-06 11:06:56 +0700

    ed step13

--- Step1/server/models/server.js
+++ Step1/server/models/server.js
@@ -9,6 +9,7 @@ class Server {
     this.paths = {
       auth: "/api/auth",
       homepage: "/api/homepage",
+      users: "/api/users",
     };
 
     this.middlewares();
@@ -19,21 +20,28 @@ class Server {
     this.app.use(cors());
     this.app.use(express.json());
 
+    /*
     // Pick up React index.html file
     this.app.use(
       express.static(path.join(__dirname, "../../client/build"))
     );
+   */
   }
 
   // Bind controllers to routes
   routes() {
     this.app.use(this.paths.auth, require("../routes/auth"));
     this.app.use(this.paths.homepage, require("../routes/homepage"));
+    this.app.use(this.paths.users, require("../routes/users"));
+
     // Catch all requests that don't match any route
     this.app.get("*", (req, res) => {
+      /*
       res.sendFile(
         path.join(__dirname, "../../client/build/index.html")
       );
+      */
+      res.send("<h1>Hello, This is API Back-end of Thanit.</h1>");
     });
   }
 
